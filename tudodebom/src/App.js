0661041f9b5ebac2ad6c7a93132679687b762236
Rafael Teixeira Paulino <rafaeltpaulino5@gmail.com>
2026-05-18 10:12:24 -0300

    Convert App component to class and fetch data

--- tudodebom/src/App.js
+++ tudodebom/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topo from "./components/Topo";
 import Home from "./components/Home";
@@ -6,20 +6,50 @@ import Receitas from "./components/Receitas";
 import Rodape from "./components/Rodape";
 import Usuarios from "./components/Usuarios";
 import Categorias from "./components/Categorias";
+import axios from 'axios';
 
-const App = () => {
+
+class App extends Component {
+  state = { 
+    receitas: [],
+    usuarios: [],
+    categorias: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const {data : receitas} = await axios.get('/api/receitas.json');
+      const {data : usuarios} = await axios.get('/api/usuarios.json');
+      const {data : categorias} = await axios.get('/api/categorias.json');
+
+      this.setState({receitas});
+      this.setState({usuarios});
+      this.setState({categorias});
+    } catch (error) {
+      console.log(error);
+      document.querySelectorAll('.principal')[0].insertAdjacentHTML(
+        "beforeend",
+        "<p class'erro'>Mensagem de erro</p>"
+      );
+    } finally {
+      console.log(this.state.receitas);
+    }
+  }
+
+  render() {
   return (
     <Router>
       <Topo />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/receitas" element={<Receitas />} />
-        <Route path="/usuarios" element={<Usuarios />} />
-        <Route path="/categorias" element={<Categorias />} />
+        <Route path="/" element={<Home receitas={this.state.receitas} />} />
+        <Route path="/receitas" element={<Receitas receitas={this.state.receitas} />} />
+        <Route path="/usuarios" element={<Usuarios usuarios={this.state.usuarios} />} />
+        <Route path="/categorias" element={<Categorias categorias={this.state.categorias} />} />
       </Routes>
       <Rodape />
     </Router>
   );
-};
+}
+}
 
 export default App;
