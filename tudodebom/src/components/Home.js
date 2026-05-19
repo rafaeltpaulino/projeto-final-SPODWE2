cf2d1c8ec59f0d23a31938d9ed2557df4435164d
Kethelyn Alves <141461344+KethelynAlves@users.noreply.github.com>
2026-05-18 22:17:11 -0300

    Add files via upload

--- tudodebom/src/components/Home.js
+++ tudodebom/src/components/Home.js
@@ -4,7 +4,15 @@ const Home = () => {
   return (
     <main className="principal">
       <h2>Bem-vindo(a) ao Compartilhamento de Receitas!</h2>
-      <p>Utilize o menu acima para gerenciar as Receitas, Categorias e Usuários do sistema.</p>
+      <p>Seja bem-vindo ao nosso espaço gastronômico! Mais do que um simples sistema de gerenciamento, 
+        este é o seu novo caderno de receitas digital — um lugar feito para guardar aqueles segredos culinários 
+        que passam de geração em geração, as descobertas de novos sabores e os pratos que reúnem a família ao redor 
+        da mesa.</p>
+      <p>
+        Cozinhar é misturar técnica com sentimento. Por isso, criamos esta plataforma para que você possa organizar 
+        suas inspirações diárias, planejar seus jantares especiais e ter sempre à mão o passo a passo daquela sobremesa 
+        inesquecível ou do prato principal perfeito.</p>
+      <p>Portanto vamos começar! Utilize o menu acima para gerenciar as Receitas, Categorias e Usuários do sistema.</p>
     </main>
   );
 };
