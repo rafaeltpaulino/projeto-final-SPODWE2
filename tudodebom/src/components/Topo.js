3d8436db771180526a69f7d3c759d7cca825753d
Vinícius Tiago de Santana <168845529+EnterName23@users.noreply.github.com>
2026-05-28 18:35:54 -0300

    Update Topo.js

--- tudodebom/src/components/Topo.js
+++ tudodebom/src/components/Topo.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navegacao from "./Navegacao";
 import Logo from "./Logo";
 
 const Topo = (props) => {
+  const [rolagem, setRolagem] = useState(false);
+
+  useEffect(() => {
+   const lidarComRolagem = () => {
+      if (window.scrollY > 100) {
+        setRolagem(true); 
+      } else if (window.scrollY < 50) {
+        setRolagem(false);
+      }
+    };
+
+    window.addEventListener('scroll', lidarComRolagem);
+        return () => {
+      window.removeEventListener('scroll', lidarComRolagem);
+    };
+  }, []);
+
   return (
-    <header className="topo">
+    <header className={`topo ${rolagem ? 'topo-scroll' : ''}`}>
       <Logo />
       <Navegacao logado={props.logado}/>
     </header>
