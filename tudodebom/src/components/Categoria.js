facd2867f3a3d131aec5d0b66bc2c1e20fe4c7ac
Vinícius Tiago de Santana <168845529+EnterName23@users.noreply.github.com>
2026-05-28 18:28:13 -0300

    Update Categoria.js

--- tudodebom/src/components/Categoria.js
+++ tudodebom/src/components/Categoria.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CardReceita from './CardReceita'; // Importando o card modularizado
 
 const Categoria = (props) => {
   const categoriaNome = props.categoria.nome;
-  const [expandido, setExpandido] = useState(false);
 
   return (
     <div className='principal'>
@@ -21,62 +21,12 @@ const Categoria = (props) => {
 
       <div className='cards-container'>
         {props.receitas.filter((receita) => receita.categoria === categoriaNome).map(r => (
-          <div key={r.id} className="card">
-
-            <img src={'/' + r.imagem} className="card-img-top" alt={r.titulo} />
-
-            {/* Corpo do Card */}
-            <div className="card-body">
-              <h5 className="card-title" >
-                {r.nome} 
-              </h5>
-              <p className="card-text-categoria">
-                {r.categoria}
-              </p>
-              <p className="card-text-autor">
-                {r.autor}
-              </p>
-
-              <CardReceita key={r.id} receita={r} className="modal-janela" />
-
-            </div>
-          </div>
+          /* Renderiza o componente CardReceita, que já tem toda a lógica de imagens, hover e modais */
+          <CardReceita key={r.id} receita={r} />
         ))}
       </div>
     </div>
   );
 };
-
-function CardReceita({ receita }) {
-  const [expandido, setExpandido] = useState(false);
-  const navigate = useNavigate();
-
-  const ReceitaCompleta = () => {
-    navigate(`/receitas/${receita.id}`);
-  }
-
-  return (
-    <div className="card-acoes">
-      <span className="card-tempo">⏱️ {receita.tempo}</span>
-      <button onClick={() => setExpandido(true)} className="btn-ver">Ver Ingredientes </button>
-
-      {expandido && (
-        <div className="modal-overlay">
-          <div className="modal-janela">
-            <h2>{receita.nome}</h2>
-            <hr />
-            <p className="text-quebra-linha"><strong>Ingredientes:</strong> {receita.ingredientes} </p>
-            <hr />
-
-            <div className="modal-botoes-container">
-              <button onClick={() => setExpandido(false)} className="btn-fechar-ingredientes">Fechar Janela </button>
-              <button onClick={ReceitaCompleta} className="btn-ver">Ver Receita</button>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-}
 
 export default Categoria;
